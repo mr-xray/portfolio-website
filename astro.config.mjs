1a26a7e865e328aa4f5e07d342ddad50d45c9805
Bernhard STEIRER <bernhard.steirer.4@gmail.com>
2024-02-01 18:43:24 +0300

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "de"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+});
