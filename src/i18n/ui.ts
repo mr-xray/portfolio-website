94dee36cd327327c3f6073b7b328b85b8dc483ca
Bernhard STEIRER <bernhard.steirer.4@gmail.com>
2024-02-01 20:12:56 +0300

    add internationalization system

---
+++ src/i18n/ui.ts
@@ -0,0 +1,24 @@
+export const languages = {
+  en: "EN",
+  de: "DE",
+};
+
+export const defaultLang = "en";
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About me",
+    "nav.physics": "Physics",
+    "nav.dev": "Software",
+    "nav.blog": "Blog",
+  },
+  de: {
+    "nav.home": "Home",
+    "nav.about": "Über mich",
+    "nav.physics": "Physik",
+    "nav.dev": "Software",
+    "nav.blog": "Blog",
+  },
+} as const;
